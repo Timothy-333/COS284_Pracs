8fae1d32d596006687392df91acd9382e8be9680
Tim Tam <timothymdevilliers@gmail.com>
2023-09-07 23:35:33 +0200

    Worked on the prac

--- Prac3/book.asm
+++ Prac3/book.asm
@@ -2,13 +2,10 @@ global book
 extern malloc
 extern strcpy
 section .data
-    isbn db "12345678",0
-    title db 50
-    price dd 1
-    quantity dd 1
+    price dd 0.0
     struc book
-        c_isbn: resb 8
-        c_title: resb 8
+        c_isbn: resb 13
+        c_title: resb 50
         align 4
         c_price: resd 1
         c_quantity: resd 1
@@ -27,38 +24,26 @@ section .text
 ; RAX - struct Book *
 
 allocateBook:
-    mov [isbn], rdi
-    mov [title], rsi
+    push rdi
+    push rsi
+    push rdx
+
     movss [price], xmm0
-    mov [quantity], edx
     mov rdi, book_size
     call malloc
     mov [c], rax
-    mov rdi, [isbn]
-    mov [rax + c_isbn], rdi
-    mov rsi, [title]
-    mov [rax + c_title], rsi
+
+    pop rdx
+    pop rsi
+    pop rdi
     movss xmm0, [price]
+
+    mov rsi, [rsi]
+    mov rdi, [rdi]
+
+    mov [rax + c_isbn], rdi
+    mov [rax + c_title], rsi
     movss [rax + c_price], xmm0
-    mov edx, [quantity]
     mov [rax + c_quantity], edx
-    ; mov [isbn], rdi
-    ; mov [title], rsi
-    ; movss [price], xmm0
-    ; mov [quantity], edx
-    ; mov rdi, book_size
-    ; call malloc
-    ; mov [c], rax
-    ; lea rdi, [rax + c_isbn]
-    ; lea rsi, [isbn]
-    ; call strcpy
-    ; mov rax, [c]
-    ; lea rdi, [rax + c_title]
-    ; lea rsi, [title]
-    ; call strcpy
-    ; mov rax, [c]
-    ; movss [rax + c_price], xmm0
-    ; mov rax, [c]
-    ; mov [rax + c_quantity], edx
-    ; mov rax, [c]
+    
     ret
